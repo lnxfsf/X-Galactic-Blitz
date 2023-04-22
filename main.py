6e97a34c11a16c7913d4ba81538e47086d8fa68b
kraken-gz <igor.lerinc7@gmail.com>
2023-04-22 18:18:12 +0200

    first window
    početni, window, pygame.. prvi useful, commits coming

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+import pygame, sys, os
+from pygame.locals import *
+
+
+
+# pygame init
+pygame.init()
+
+#set up window
+WIDTH, HEIGHT = 600, 600
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("X-Galactic-Blitz")
+
+
+
+# main game loop
+while True: 
+
+
+    # checking for events 
+    for event in pygame.event.get():
+
+
+        # quit if X button pressed (in window)
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # redraw (update) screen
+    pygame.display.update()
 
