a6b0b0b882a06472a9af37964f3d93afa7696f70
kraken-gz <igor.lerinc7@gmail.com>
2023-04-23 12:20:19 +0200

    scrollable background - function
    scrollable background - function, je unutar main.py, ali ja koristim ovu
    spolja fajl, importujem funkciju iz drugog fajla, gde je definisano ovo.
    WIDTH I HEIGHT, je definisano u tom fajlu.. ako moze, bez toga, nego da
    koristi ovaj sto je u main.py , bilo bi super. sledeci commit, je da
    sklonim ovu funkciju u main.py, da samo koristi, funkciju definisanu sa
    fajla

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import pygame, sys, os
 from pygame.locals import *
-
-#handle text input
-import pygame_textinput
+import math
+from scrollable_background import scroll_bg
 
 
 # pygame init
@@ -15,20 +14,72 @@ pygame.display.set_caption("X-Galactic-Blitz")
 
 
 
+# NOVO---------------------- 
+
+# LOAD SPACESHIPS
+BLUE_SPACESHIP = pygame.image.load(os.path.join("images", "enemy.png"))
+YELLOW_SPACESHIP = pygame.image.load(os.path.join("images", "player.png"))
+
+
+
+
+#def scroll_bg(WIN):
+#    clock = pygame.time.Clock()
+#    BG = pygame.transform.scale(pygame.image.load(os.path.join("images", "background-black.png")), (WIDTH, HEIGHT))
+#    scroll = 0
+#    tiles = math.ceil(HEIGHT / BG.get_height()) + 1
+#
+#    while True:
+#        clock.tick(50)
+#
+#        i = 0
+#        while i < tiles:
+#            WIN.blit(BG, (0, scroll + BG.get_height() * i))
+#            i += 1
+#
+#        scroll -= 35
+#
+#        if abs(scroll) > BG.get_height():
+#            scroll = 0
+#
+#        for event in pygame.event.get():
+#            if event.type == pygame.QUIT:
+#                pygame.quit()
+#                sys.exit()
+#
+#        pygame.display.update()
+#
+
+# NOVO-------------------- 
+
+
 
 
 # main game loop
 while True: 
+    
 
 
-    # checking for events 
+    # scroll background
+    scroll_bg(WIN)
+
+
+
+    # checking for events ********** 
     for event in pygame.event.get():
+
+
+
 
 
         # quit if X button pressed (in window)
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+    
+
+    # checking for events ********** 
+
 
     # redraw (update) screen
     pygame.display.update()
